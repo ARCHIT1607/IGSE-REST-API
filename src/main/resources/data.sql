2ce47f6c933d7ad8d1558fc076871d3fb15a86af
Archit Maniyath <archit778@gmail.com>
2023-01-15 17:59:31 +0000

    initial data dump added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO customer (id,address,balance,bedrooms,email,password,property_type,role) VALUES(1,'123 University Road',400,1,'gse@shangrila.gov.un',
+							   '$2a$10$zJsqXlLss2t0LVf2IngWXeBjEGLSIBUIZQ3MU5dZUt2NlCKsqNiFq',
+							   'flat','ROLE_ADMIN');
