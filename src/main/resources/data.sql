32b9ba01be337d60d136bfbe9b7f8a94b9d3d69b
Archit Maniyath <archit778@gmail.com>
2023-01-15 22:48:41 +0000

    email typo in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO customer (id,address,balance,bedrooms,email,password,property_type,role) VALUES(1,'123 University Road',400,1,'test@gmail.com',
+INSERT INTO customer (id,address,balance,bedrooms,email,password,property_type,role) VALUES(1,'123 University Road',400,1,'gse@shangrila.gov.un',
 							   '$2a$10$8RJIMzuSclTsTbLTqWt42OmqPucA/P2ZDs3.jatwkMOlGFo2PqRfm',
 							   'flat','ROLE_ADMIN');
