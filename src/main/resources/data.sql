35691fddaa2c6e112326c93e5bfbe00468537379
Archit Maniyath <archit778@gmail.com>
2023-01-15 20:32:10 +0000

    removed data.sql

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO customer (id,address,balance,bedrooms,email,password,property_type,role) VALUES(1,'123 University Road',400,1,'gse@shangrila.gov.un',
-							   '$2a$10$zJsqXlLss2t0LVf2IngWXeBjEGLSIBUIZQ3MU5dZUt2NlCKsqNiFq',
-							   'flat','ROLE_ADMIN');
