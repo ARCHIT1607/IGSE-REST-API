91ebc2002b32124af53de1afad57b2b996ab05db
Archit Maniyath <archit778@gmail.com>
2023-01-16 09:01:36 +0000

    testing

--- src/main/java/com/iGSE/service/UserDetailsServiceImpl.java
+++ src/main/java/com/iGSE/service/UserDetailsServiceImpl.java
@@ -19,9 +19,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		
+		System.out.println("email "+email);
 		Customer user=userRepo.findByEmail(email);
 		
-//		System.out.println("inside impl "+user);
+		System.out.println("inside impl "+user);
 		
 		if(user==null) {
 			System.out.println("exception thrown");
